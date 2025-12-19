06c17909c5b71eaa6dfc5e88aeedb9d3cbe7e47e
MarcellFer <marcellfernandowongso@gmail.com>
2025-12-19 14:10:35 +0700

    Project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p>
+      <p>Selamat datang, tempat di mana inspirasi kuliner bertemu dengan kelezatan yang tak terbatas. Kami hadir untuk membantu Anda mengeksplorasi berbagai resep makanan yang mudah, lezat, dan penuh cita rasa. Apakah Anda seorang koki handal atau pemula di dapur, di sini Anda akan menemukan berbagai pilihan resep yang sesuai dengan kebutuhan dan selera Anda.</p>
     </div>
   );
 }
